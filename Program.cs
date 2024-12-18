1d6fec5d3980cafcd31d56ab434b80807d43a310
Nhlakanipho Khoza 2 <nhlakanipho.khoza2@durban.gov.za>
2024-12-18 20:30:00 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = "one two three four five";
+        string reversedString = string.Join(" ", inputString
+            .Split(' ')
+            .Select(x => new string(x.Reverse().ToArray())));
+        
+        Console.WriteLine(reversedString);
+    }
+}
